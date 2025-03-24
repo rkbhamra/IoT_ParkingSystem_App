fe8d6ce68a0e87fb9a824108c4b34b3d99c6e1d3
reetkaurbhamra <reet.rkb@gmail.com>
2025-03-24 13:59:47 -0400

    updating parking get() request to get latest data each time the get request is called

--- backend/routes/parking.py
+++ backend/routes/parking.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify
+from . import utils
 
 parking_bp = Blueprint('parking', __name__)
 
-
 @parking_bp.route('/get', methods=['GET'])
 def get():
-    return jsonify({'message': 'get parking test'})
+    utils.run_file("backend\\routes\\getData.py")
+    return utils.get_db("backend\\databases\\lot_data.json")
+
--- backend/routes/utils.py
+++ backend/routes/utils.py
@@ -1,5 +1,5 @@
 import json
-
+import subprocess
 
 def get_db(file):
     with open('databases/' + file, 'r') as f:
@@ -9,3 +9,10 @@ def get_db(file):
 def save_db(file, data):
     with open('databases/' + file, 'w') as f:
         json.dump(data, f, indent=4)
+
+def run_file(file):
+    result = subprocess.run(['python', file], capture_output=True, text=True)
+    if result.returncode == 0:
+        return {"status": "success", "output": result.stdout}
+    else:
+        return {"status": "error", "error": result.stderr}
