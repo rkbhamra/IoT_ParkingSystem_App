cd42a2316a9a3bea73ba7c91980e00ad01cae045
Reet Bhamra <66404210+rkbhamra@users.noreply.github.com>
2025-03-31 16:16:29 -0400

    Update 892_PoC.ino

--- Arduino Sketches/892_PoC.ino
+++ Arduino Sketches/892_PoC.ino
@@ -4,6 +4,11 @@
 #define PHOTORESISTOR1_PIN 32  
 #define PHOTORESISTOR2_PIN 35 
 #define PHOTORESISTOR3_PIN 33  
+#define PHOTORESISTOR4_PIN 2  
+#define PHOTORESISTOR5_PIN 4  
+#define PHOTORESISTOR6_PIN 12  
+#define PHOTORESISTOR7_PIN 13  
+
 #define LIGHT_THRESHOLD 2000   
 
 void setup() {
@@ -15,6 +20,11 @@ void setup() {
 
   setDebugMessageLevel(2);
   ArduinoCloud.printDebugInfo();
+  
+  pinMode(PHOTORESISTOR4_PIN, INPUT);
+  pinMode(PHOTORESISTOR5_PIN, INPUT);
+  pinMode(PHOTORESISTOR6_PIN, INPUT);
+  pinMode(PHOTORESISTOR7_PIN, INPUT);
 }
 
 void loop() {
@@ -31,6 +41,10 @@ void loop() {
     photoResistor1 = (light1 > LIGHT_THRESHOLD);
     photoResistor2 = (light2 > LIGHT_THRESHOLD);
     photoResistor3 = (light3 > LIGHT_THRESHOLD);
+    photoResistor4 = digitalRead(PHOTORESISTOR4_PIN);
+    photoResistor5 = digitalRead(PHOTORESISTOR5_PIN);
+    photoResistor6 = digitalRead(PHOTORESISTOR6_PIN);
+    photoResistor7 = digitalRead(PHOTORESISTOR7_PIN);
 
     // Debugging output
     Serial.print("Light Level 0: ");
@@ -52,4 +66,24 @@ void loop() {
     Serial.print(light3);
     Serial.print(" -> ");
     Serial.println(photoResistor3 ? "HIGH" : "LOW");
+
+    Serial.print("Light Level 4: ");
+    Serial.print(photoResistor4);
+    Serial.print(" -> ");
+    Serial.println(photoResistor4 ? "HIGH" : "LOW");
+
+    Serial.print("Light Level 5: ");
+    Serial.print(photoResistor5);
+    Serial.print(" -> ");
+    Serial.println(photoResistor5 ? "HIGH" : "LOW");
+
+    Serial.print("Light Level 6: ");
+    Serial.print(photoResistor6);
+    Serial.print(" -> ");
+    Serial.println(photoResistor6 ? "HIGH" : "LOW");
+
+    Serial.print("Light Level 7: ");
+    Serial.print(photoResistor7);
+    Serial.print(" -> ");
+    Serial.println(photoResistor7 ? "HIGH" : "LOW");
 }
